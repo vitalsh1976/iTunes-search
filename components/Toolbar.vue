47c300b2584293005421c3a2a99d93b1c162cb3d
vitalsh1976 <vitalsh189@gmail.com>
2019-05-22 16:04:22 +0300

    first commit

---
+++ components/Toolbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-toolbar dark color="blue">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>
+      <nuxt-link class="title" to="/">iTunes Search</nuxt-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn to="/" icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style>
+.title {
+  text-decoration: none !important;
+}
+.title:visited {
+  color: white;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Search Itunes</h1>
+    <br>
+    <form @submit.prevent="submit">
+      <input placeholder="Enter artist name" v-model="search" ref="search" autofocus>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: ""
+    };
+  },
+  methods: {
+    submit(event) {
+      this.$router.push(`results/${this.search}`);
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  text-align: center;
+}
+h1 {
+  padding: 20px;
+}
+</style>
+
